23919e4f37b3f6ee7138f8976377ef1a185ad81f
DerRight <zxc123456789123456@gmail.com>
2024-06-21 23:17:09 +0800

    將程式碼修正並且補上註解

--- grabvotesrobot.py
+++ grabvotesrobot.py
@@ -1,27 +1,40 @@
 from selenium import webdriver
-driver = webdriver.Chrome('./chromedriver')
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+# 使用 webdriver_manager 自動下載並配置 ChromeDriver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 # 引號內放要去的網址
 driver.get('https://shopping.friday.tw/ec2/product?pid=7380168&cid=418800&sid=0&mid=1')
 
+# 點擊購買按鈕
 driver.find_element_by_xpath("//*[@id=\"e3_buy\"]/span").click()
 
 # 登入帳號密碼
 account = driver.find_element_by_xpath("//*[@id=\"j_username\"]")
 account.clear()
 account.send.keys("你的帳號")
+
 password = driver.find_element_by_xpath('//*[@id=\"j_password\"]')
 password.clear()
 password.send.keys("你的密碼")
+
 driver.find_element_by_xpath("//*[@id=\"login_btn\"]").click()
 
+# 點擊購買
 driver.find_element_by_xpath("//*[@id=\"contarea-box\"]/ul/li/div[3]/button[1]/span").click()
 
+# 輸入信用卡驗證碼
 cvc = driver.find_element_by_xpath("//*[@id=\"creidtcard_record_area_default\"]/div/div[2]/input[2]").click()
 cvc.clear()
 cvc.send.keys("信用卡驗證碼")
 
 driver.find_element_by_xpath("//*[@id=\"same\"]")
+
+# 填寫地址
 homee2 = driver.find_element_by_xpath("//*[@id=\"shippingaddress2\"]")
 homee2.clear()
 homee2.send.keys("地址")
@@ -32,23 +45,36 @@ driver.find_element_by_xpath("//*[@id=\*contarea-box*\"]/div[3]/button").click()
 namee = driver.find_element_by_xpath("//*[@id=\"billingname\"]")
 namee.clear()
 namee.send.keys("姓名")
+
 phonee = driver.find_element_by_xpath("//*[@id=\"billingmobile\"]")
 phonee.clear()
 phonee.send.keys("電話")
+
 eemail = driver.find_element_by_xpath("//*[@id=\"billingemail\"]")
 eemail.clear()
 eemail.send.keys("電子郵件")
+
 homee = driver.find_element_by_xpath("//*[@id=\"addConsigneeCityId2\"]")
 homee.send.keys("縣市")
+
 year = driver.find_element_by_xpath("//*[@id=\"birthbay_year\"]")
 year.send.keys("西元出生年")
+
 month = driver.find_element_by_xpath("//*[@id=\"birthbay_month\"]")
 month.send.keys("月份不含0")
+
 day = driver.find_element_by_xpath("//*[@id=\"birthbay_day\"]")
 day.send.keys("日期不含0")
+
 homee2 = driver.find_element_by_xpath("//*[@id=\"address3\"]")
 homee2.send.keys("地址")
+
 # 下面女生的話改成gender2
-driver.find_element_by_xpath("//*[@id=\"gender1\"]").click()
+driver.find_element_by_xpath("//*[@id=\"gender1\"]").click() # 男性
+# driver.find_element_by_xpath("//*[@id=\"gender2\"]").click() # 女性
+
 driver.find_element_by_xpath("//*[@id=\"same\"]").click()
 driver.find_element_by_xpath("//*[@id=\"same2\"]").click()
+
+# 關閉瀏覽器
+driver.quit()
